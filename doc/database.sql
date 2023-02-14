658090dc8632769179b57142578a78a0867291b5
omoi iwasawa <maydayniuzhuan@gmail.com>
2023-02-14 13:54:21 +0800

    add(doc): create database table sql

---
+++ doc/database.sql
@@ -0,0 +1,58 @@
+-- 用户表
+create table user
+(
+    id       integer primary key autoincrement,
+    username text unique,
+    password text,
+    role     int,
+    user_id  int
+);
+-- 学院表
+create table faculty
+(
+    id           integer primary key autoincrement,
+    faculty_name text
+);
+-- 学生表
+create table student
+(
+    student_id       int primary key,
+    student_name     text,
+    student_gender   text check (student_gender in ('男', '女')),
+    student_birthday text,
+    student_register text,
+    student_belong   int
+);
+-- 教师表
+create table teacher
+(
+    teacher_id        int primary key,
+    teacher_name      text,
+    teacher_gender    text check (teacher_gender in ('男', '女')),
+    teacher_birthday  text,
+    teacher_education text,
+    teacher_rank      text,
+    teacher_register  text,
+    teacher_belong    int
+);
+-- 课程表
+create table course
+(
+    course_id      int primary key,
+    course_name    text,
+    course_date    text,
+    course_address text,
+    course_times   int,
+    course_type    int check (course_type in (0, 1)),
+    course_point   int,
+    teacher_id     int,
+    course_belong  int
+);
+-- 成绩表
+create table score
+(
+    id         int primary key,
+    course_id  int,
+    student_id int,
+    score      int
+)
